da074137207afca918b8d9a1066838bbf4530b87
Eavid <ganrn@vip.qq.com>
2024-01-12 20:51:57 +0800

    chore: 构建流程更改

---
+++ rollup.config.js
@@ -0,0 +1,24 @@
+import esmTsPlugin from "@rollup/plugin-typescript";
+import { defineConfig } from "rollup";
+
+const tsPlugin = esmTsPlugin;
+const output = "dist/mod.js";
+
+export default defineConfig({
+  input: "./src/mod.ts",
+  output: {
+    file: output,
+  },
+  plugins: [
+    tsPlugin({
+      include: ["./src/**"],
+      compilerOptions: {
+        target: "ES2022",
+        module: "nodenext",
+        declaration: true,
+        declarationDir: "dist",
+        declarationMap: true,
+      },
+    }),
+  ],
+});
