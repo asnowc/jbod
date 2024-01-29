731bf920dd338a2d29c213b606c434aa3221434c
Eavid <ganrn@vip.qq.com>
2024-01-29 17:42:17 +0800

    refactor: utf8 编码

--- src/uint_array_util/string.ts
+++ src/uint_array_util/string.ts
@@ -1,46 +1,40 @@
-/**
- * 这个库本意是不依赖非 JavaScript 标准就接口。
- * 但是这个字符串编码速度太慢。。。
- * 所以这里检测 TextDecoder 和 TextEncoder 接口是否支持
- */
-
-export function readByUtf8(buf: Uint8Array) {
-  let str = "";
-  let code: number;
-  for (let i = 0; i < buf.length; ) {
-    let utf8Byte = buf[i];
-    if (utf8Byte < 0b10000000) {
-      code = utf8Byte;
-      i++;
-    } else if (utf8Byte < 0b11100000) {
-      code = ((utf8Byte & 0b11111) << 6) + (buf[i + 1] & 0b111111);
-      i += 2;
-    } else if (utf8Byte < 0b11110000) {
-      code = ((utf8Byte & 0b1111) << 12) + (((buf[i + 1] & 0b111111) << 6) + (buf[i + 2] & 0b111111));
-      i += 3;
-    } else if (utf8Byte < 0b11111000) {
-      code =
-        ((utf8Byte & 0b111) << 18) +
-        (((buf[i + 1] & 0b111111) << 12) + (((buf[i + 2] & 0b111111) << 6) + (buf[i + 3] & 0b111111)));
-      i += 4;
-    } else if (utf8Byte < 0b11111100) {
-      code =
-        ((utf8Byte & 0b11) << 24) +
-        ((buf[i + 1] & 0b111111) << 18) +
-        (((buf[i + 2] & 0b111111) << 12) + ((buf[i + 3] & 0b111111) << 6) + (buf[i + 4] & 0b111111));
-      i += 5;
+export function readByUtf8(buf: Uint8Array, chunk: number[]) {
+  let y = 0;
+  let str = [];
+  let strIndex = 0;
+  let code;
+  let i = 0;
+  while (i < buf.length) {
+    code = buf[i++];
+    if (code < 0b10000000) {
+      chunk[y++] = code;
+    } else if (code < 0b11100000) {
+      chunk[y++] = ((code & 0b11111) << 6) | (buf[i++] & 63);
+    } else if (code < 0b11110000) {
+      chunk[y++] = ((code & 0b1111) << 12) | (((buf[i++] & 63) << 6) | (buf[i++] & 63));
+    } else if (code < 0b11111000) {
+      chunk[y++] = ((code & 0b111) << 18) | (((buf[i++] & 63) << 12) | (((buf[i++] & 63) << 6) | (buf[i++] & 63)));
+    } else if (code < 0b11111100) {
+      chunk[y++] =
+        ((code & 0b11) << 24) |
+        ((buf[i++] & 63) << 18) |
+        (((buf[i++] & 63) << 12) | ((buf[i++] & 63) << 6) | (buf[i++] & 63));
     } else {
-      code =
-        ((utf8Byte & 0b1) << 30) +
-        ((buf[i + 1] & 0b111111) << 24) +
-        ((buf[i + 1] & 0b111111) << 18) +
-        (((buf[i + 2] & 0b111111) << 12) + ((buf[i + 3] & 0b111111) << 6) + (buf[i + 4] & 0b111111));
-      i += 6;
+      chunk[y++] =
+        ((code & 0b1) << 30) |
+        ((buf[i++] & 63) << 24) |
+        ((buf[i++] & 63) << 18) |
+        (((buf[i++] & 63) << 12) | ((buf[i++] & 63) << 6) | (buf[i++] & 63));
     }
-    str += String.fromCharCode(code); //Very slow
+    if (y >= chunk.length) {
+      str[strIndex++] = String.fromCharCode.apply(String, chunk);
+      y = 0;
+    }
   }
-  return str;
+  if (str.length) return str.join("");
+  return String.fromCharCode.apply(String, y === chunk.length ? chunk : chunk.slice(0, y));
 }
+
 export function calcUtf8Length(str: string) {
   let utf8Len = 0;
   let code: number;
@@ -55,69 +49,47 @@ export function calcUtf8Length(str: string) {
   }
   return utf8Len;
 }
-
-export function writeByUtf8Into(str: string, buf: Uint8Array) {
-  let code: number;
-  let offset = 0;
+export function writeByUtf8Into(str: string, buf: Uint8Array, offset = 0) {
+  let code;
   for (let i = 0; i < str.length; i++) {
     code = str.charCodeAt(i);
     if (code < 0x80) buf[offset++] = code;
     else if (code < 0x8_00) {
-      buf[offset + 1] = 0b10000000 + (code & 0b111111);
-      code >>= 6;
-      buf[offset] = 0b11000000 + code;
-      offset += 2;
+      buf[offset++] = 0b11000000 | (code >> 6);
+      buf[offset++] = 128 | (63 & code);
     } else if (code < 0x10000) {
-      buf[offset + 2] = 0b10000000 + (code & 0b111111);
-      code >>= 6;
-      buf[offset + 1] = 0b10000000 + (code & 0b111111);
-      code >>= 6;
-      buf[offset] = 0b11100000 + code;
-      offset += 3;
+      buf[offset++] = 0b11100000 | (63 & (code >> 12));
+      buf[offset++] = 128 | (63 & (code >> 6));
+      buf[offset++] = 128 | (63 & code);
     } else if (code < 0x200000) {
-      buf[offset + 3] = 0b10000000 + (code & 0b111111);
-      code >>= 6;
-      buf[offset + 2] = 0b10000000 + (code & 0b111111);
-      code >>= 6;
-      buf[offset + 1] = 0b10000000 + (code & 0b111111);
-      code >>= 6;
-      buf[offset] = 0b11110000 + code;
-      offset += 4;
+      buf[offset++] = 0b11110000 | (63 & (code >> 18));
+      buf[offset++] = 128 | (63 & (code >> 12));
+      buf[offset++] = 128 | (63 & (code >> 6));
+      buf[offset++] = 128 | (63 & code);
     } else if (code < 0x4000000) {
-      buf[offset + 4] = 0b10000000 + (code & 0b111111);
-      code >>= 6;
-      buf[offset + 3] = 0b10000000 + (code & 0b111111);
-      code >>= 6;
-      buf[offset + 2] = 0b10000000 + (code & 0b111111);
-      code >>= 6;
-      buf[offset + 1] = 0b10000000 + (code & 0b111111);
-      code >>= 6;
-      buf[offset] = 0b11111000 + code;
-      offset += 5;
+      buf[offset++] = 0b11111000 | (63 & (code >> 24));
+      buf[offset++] = 128 | (63 & (code >> 18));
+      buf[offset++] = 128 | (63 & (code >> 12));
+      buf[offset++] = 128 | (63 & (code >> 6));
+      buf[offset++] = 128 | (63 & code);
     } else {
-      buf[offset + 5] = 0b10000000 + (code & 0b111111);
-      code >>= 6;
-      buf[offset + 4] = 0b10000000 + (code & 0b111111);
-      code >>= 6;
-      buf[offset + 3] = 0b10000000 + (code & 0b111111);
-      code >>= 6;
-      buf[offset + 2] = 0b10000000 + (code & 0b111111);
-      code >>= 6;
-      buf[offset + 1] = 0b10000000 + (code & 0b111111);
-      code >>= 6;
-      buf[offset] = 0b11111100 + code;
-      offset += 6;
+      buf[offset++] = 0b11111100 | (63 & (code >> 32));
+      buf[offset++] = 128 | (63 & (code >> 24));
+      buf[offset++] = 128 | (63 & (code >> 18));
+      buf[offset++] = 128 | (63 & (code >> 12));
+      buf[offset++] = 128 | (63 & (code >> 6));
+      buf[offset++] = 128 | (63 & code);
     }
   }
-  return buf;
+  return offset;
 }
-
+const cache = new Array(4095);
 export const decodeUtf8: (buf: Uint8Array) => string = (function () {
   const TextDecoder = (globalThis as any).TextDecoder;
   if (TextDecoder) {
     const textDecoder = new TextDecoder();
     return (buf: Uint8Array) => textDecoder.decode(buf);
-  } else return readByUtf8;
+  } else return (buf) => readByUtf8(buf, cache);
 })();
 
 export const encodeUtf8Into: (str: string, buf: Uint8Array) => void = (function () {
