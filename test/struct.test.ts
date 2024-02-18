fed6f27dc19fa11ab3638f1b34be0a0ea0c98939
Eavid <ganrn@vip.qq.com>
2024-02-18 18:48:23 +0800

    test: 结构体简单测试

--- test/struct.test.ts
+++ test/struct.test.ts
@@ -2,19 +2,28 @@ import { DataType, StructEncoder } from "jbod";
 import { describe, expect, test } from "vitest";
 import { formatBin } from "./utils/mod.js";
 describe("encode", function () {
-  const s1 = StructEncoder.define<{ f1: boolean; f2: bigint; f3: any }>({
-    f1: { id: 1, type: DataType.true },
-    f2: { id: 2, type: DataType.i64 },
-    f3: 3,
-  });
+  const s1 = StructEncoder.define<{ f1: boolean; f2: number; f3: any }>(
+    {
+      f1: { id: 1, type: DataType.true },
+      f2: { id: 2, type: DataType.i32 },
+      f3: 3,
+    },
+    { required: true }
+  );
   test("encode", function () {
-    const u8Arr = s1.encode({ f1: true, f2: 9n, f3: "ab" });
-    expect(formatBin(u8Arr)).toBe(`010102000000000000000903026162`);
-  });
-  test("类型不一致", function () {
-    expect(() => s1.encode({ f1: true, f2: "", f3: "ab" } as any)).toThrowError();
+    const u8Arr = s1.encode({ f1: true, f2: 9, f3: "ab" });
+    expect(formatBin(u8Arr)).toBe(`01010200000009030a02616200`);
   });
   test("字段缺失", function () {
     expect(() => s1.encode({ f1: true, f2: 9n } as any)).toThrowError();
+  });
+  test("可选类型", function () {
+    const s1 = StructEncoder.define<Partial<{ f1: boolean; f2: number; f3: any }>>({
+      f1: { id: 1, type: DataType.true },
+      f2: { id: 2, type: DataType.i32 },
+      f3: 3,
+    });
+    const u8Arr = s1.encode({ f2: 9 });
+    expect(formatBin(u8Arr)).toBe(`020000000900`);
   });
 });
--- test/utils/mod.ts
+++ test/utils/mod.ts
@@ -1,4 +1,4 @@
 export function formatBin(buf: Uint8Array) {
   const buffer = Buffer.from(buf, buf.byteOffset, buf.byteLength);
-  return buffer.toString("binary");
+  return buffer.toString("hex");
 }
